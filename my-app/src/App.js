5f3eafa72c17cb44c645fd30db8603dc2c875f7d
Edward Mahama <edwardmahama@microsoft.com>
2023-03-26 20:59:53 -0400

    one-way-flow of data

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,23 @@
 import { useState } from "react";
 import "./App.css";
 
-function Button() {
-  const [counter, setCounter] = useState(0);
-  return <button onClick={() => setCounter(counter + 1)}>{counter}</button>;
+function Button(props) {
+  // const handleClick = () => setCounter(counter + 1);
+  return <button onClick={props.onClickFunction}>+1</button>;
+}
+
+function Display(props) {
+  return <div>{props.message}</div>;
 }
 function App() {
-  return <Button />;
+  const [counter, setCounter] = useState(43);
+  const incrementCounter = () => setCounter(counter + 1);
+  return (
+    <div>
+      <Button onClickFunction={incrementCounter} />
+      <Display message={counter} />
+    </div>
+  );
 }
 
 export default App;
