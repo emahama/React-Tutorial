f88ad9bd11237b705e029dc1b30a3e6e809f53f0
Edward Mahama <edwardmahama@microsoft.com>
2023-03-26 21:11:36 -0400

    add increment property

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,19 +2,21 @@ import { useState } from "react";
 import "./App.css";
 
 function Button(props) {
-  // const handleClick = () => setCounter(counter + 1);
-  return <button onClick={props.onClickFunction}>+1</button>;
+  const handleClick = () => props.onClickFunction(props.increment);
+  return <button onClick={handleClick}>+{props.increment}</button>;
 }
 
 function Display(props) {
   return <div>{props.message}</div>;
 }
 function App() {
-  const [counter, setCounter] = useState(43);
-  const incrementCounter = () => setCounter(counter + 1);
+  const [counter, setCounter] = useState(0);
+  const incrementCounter = (incrementVal) => setCounter(counter + incrementVal);
   return (
     <div>
-      <Button onClickFunction={incrementCounter} />
+      <Button onClickFunction={incrementCounter} increment={5} />
+      <Button onClickFunction={incrementCounter} increment={10} />
+      <Button onClickFunction={incrementCounter} increment={15} />
       <Display message={counter} />
     </div>
   );
