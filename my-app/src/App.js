57139693f5ede24716272f0f166e3ab43cb009ea
Edward Mahama <edwardmahama@microsoft.com>
2023-03-28 09:28:15 -0400

    add Card and Cards componebt

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,70 @@
+import React from "react";
 import "./App.css";
 
-function Hello() {
-  return <div className="App">Hello React !!.</div>;
+// Test Data
+
+const testData = [
+  {
+    name: "Dan Abramov",
+    avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4",
+    company: "@facebook",
+  },
+  {
+    name: "Sophie Alpert",
+    avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4",
+    company: "Humu",
+  },
+  {
+    name: "Sebastian Markbåge",
+    avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4",
+    company: "Facebook",
+  },
+];
+
+// const App = ({ title }) => {
+//   return <div className="header">{title}</div>;
+// };
+
+// Card Component
+
+class Card extends React.Component {
+  render() {
+    return (
+      <div className="github-profile">
+        <img src={this.props.avatar_url} alt="" />
+        <div className="info">
+          <div className="name">{this.props.name}</div>
+          <div className="company">{this.props.company}</div>
+        </div>
+      </div>
+    );
+  }
 }
-function App() {
-  return <Hello />;
+
+// Cards Component
+
+class Cards extends React.Component {
+  render() {
+    const profile1 = { ...testData[0] };
+    const profile2 = { ...testData[1] };
+    return (
+      <div>
+        <Card {...profile1} />
+        <Card {...profile2} />
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="header">{this.props.title}</div>;
+        <Cards />;
+      </div>
+    );
+  }
 }
 
 export default App;
